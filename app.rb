0b0bd730395b1986b15cbc189d774f069016ad07
edmundo papi <edmundopierre@yahoo.com>
2020-09-08 15:53:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,6 +25,23 @@ get "/say/:word1/:word2/:word3/:word4/:word5" do
  str = ""
  @user = str +params[:word1]+" "+params[:word2]+" "+params[:word3]+" "+params[:word4]+" "+params[:word5]+"."
  @user
+end
 
+get "/:operation/:number1/:number2" do 
+  @oper = paramas[:operation]
+  @num1 = params[:number1]
+  @num2 = paras[:number2]
+
+  case @oper 
+  when "subtract"
+    "#{@num1 - @num2}to_s"
+
+  when"add"
+    "#{@num1+@num2}to_s"
+  when"multiply"
+    "#{@num1 * @num2}to_s"
+  when"divide"
+    "#{@num1 / @num2}to_s"
+  end
 end
 end
